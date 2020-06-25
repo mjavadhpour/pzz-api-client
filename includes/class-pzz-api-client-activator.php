3ec243fdff24d988875faae99173962678293c24
Mohammad Zare Moghadam <mzmoghadam72@gmail.com>
2020-06-26 01:54:02 +0430

    fix(activation): remove conflict warning
    remove conflict warning to reactor-core plugin

--- includes/class-pzz-api-client-activator.php
+++ includes/class-pzz-api-client-activator.php
@@ -19,30 +19,8 @@ class Pzz_Api_Client_Activator {
 	 *
 	 * @since    1.0.0
 	 */
-	public static function activate() {
-
-		/**
-		 * Detect plugin. For use in Admin area only.
-		 */
-		if ( is_plugin_active( 'reactor-core/reactor-core.php' ) ) {
-			wp_die(
-				'<h1>Oops! 🤦‍♂️</h1>' .
-				'<p>' . sprintf(
-					__( 'Our plugin has a conflict with <a href="%s">Reactor: Core</a>. <b>please remove or deactivate it and try again.</b>' ),
-					'https://wordpress.org/plugins/reactor-core/'
-				) . '</p>'
-				. '<p>' .
-					__( 'This is because some of our functions and APIs have an internal conflict with this plugin, we sorry about that and this will be fixed later.' )
-				. '</p>' 
-				. '<p>' .
-				sprintf(
-					__('<a href="%s">Back</a>'),
-					self_admin_url( "plugins.php" )
-				)
-				. '</p>' 
-			);
-		} 
-
+	public static function activate()
+	{
+		// doing actions that are required for activation
 	}
-
 }
